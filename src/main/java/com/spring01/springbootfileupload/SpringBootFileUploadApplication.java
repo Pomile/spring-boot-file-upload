0bcd0bab79f078d65968f2155b1b87529d70ab05
Babatunde <soft-sky@live.co.uk>
2019-11-11 07:42:18 +0100

    feat[file-upload]: Implement file upload
    [x] Create UploadController
    [x] Create static file - index.html
    [x] Modify SpringBootFileUploadApplication

--- src/main/java/com/spring01/springbootfileupload/SpringBootFileUploadApplication.java
+++ src/main/java/com/spring01/springbootfileupload/SpringBootFileUploadApplication.java
@@ -2,8 +2,10 @@ package com.spring01.springbootfileupload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class SpringBootFileUploadApplication {
 
 	public static void main(String[] args) {
